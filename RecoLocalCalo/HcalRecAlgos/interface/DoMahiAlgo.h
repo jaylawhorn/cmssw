894c343ef7e002ab22c60c8f80fb06d406288be4
Jay <jlawhorn@mit.edu>
2017-10-18 11:05:30 +0200

    add MAHI

--- RecoLocalCalo/HcalRecAlgos/interface/DoMahiAlgo.h
+++ RecoLocalCalo/HcalRecAlgos/interface/DoMahiAlgo.h
@@ -12,14 +12,63 @@
 class DoMahiAlgo
 {
  public:
+
+  typedef BXVector::Index Index;
   
   DoMahiAlgo();
   ~DoMahiAlgo() { };
 
-  //  void phase1Apply(const HBHEChannelInfo& channelData, float& reconstructedEnergy, float& chi2);
-  //  void setPulseShapeTemplate();
+  void phase1Apply(const HBHEChannelInfo& channelData);//, float& reconstructedEnergy, float& chi2);
+  
+  bool DoFit(SampleVector amplitudes, SampleVector gains, std::vector<float> &correctedOutput);
+
+  //void setPulseShapeTemplate();
 
  private:
+
+  bool Minimize();
+  bool UpdateCov();
+  double CalculateChiSq();
+  bool NNLS();
+  SampleVector _amplitudes;
+  SampleMatrix _invCovMat;
   
+  FullSampleMatrix noiseCor;
+  FullSampleMatrix pulseCov;
+
+  SamplePulseMatrix _pulseMat;
+
+  PulseVector _ampVec;
+  PulseVector _ampVecMin;
+  PulseVector _errVec;
+  SampleVector pulseShape;
+  SampleVector pulseShapeM;
+  SampleVector pulseShapeP;
+  SampleVector zeroShape;
+
+  PulseVector ampvecpermtest;
+
+  HcalDetId _detID;
+
+  BXVector _bxs;
+  BXVector _bxsMin;
+  unsigned int _nPulseTot;
+  unsigned int _nP;  
+
+  double _chiSq;
+
+  SamplePulseMatrix invcovp;
+  PulseMatrix aTaMat; // A-transpose A (matrix)
+  PulseVector aTbVec; // A-transpose b (vector)
+  PulseVector wVec; // w (vector)
+  PulseVector updateWork; // w (vector)
+
+  SampleDecompLLT _covDecomp;
+  SampleMatrix _covDecompLinv;
+  PulseMatrix _topleft_work;
+  PulseDecompLDLT _pulseDecomp;
+
+  //PulseShapes pulseShapeObj;
+
 }; 
 #endif
